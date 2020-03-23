66bae37125c973cb3632d4c0717a4e92c33f7741
Administrator <Administrator@PC-202003112310>
2020-03-23 22:34:06 +0800

    啊大大

--- sql/personInfo.sql
+++ sql/personInfo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `person_info` (
-  `id` int(11) GENERATED ALWAYS AS (1) STORED NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `chushengriqi` varchar(45) DEFAULT NULL,
   `xingbie` varchar(45) DEFAULT NULL,
   `zuji` varchar(45) DEFAULT NULL,
@@ -13,10 +13,11 @@ CREATE TABLE `person_info` (
   `jiatingqingkuang` varchar(45) DEFAULT NULL,
   `youxiangdizhi` varchar(45) DEFAULT NULL,
   `qitaxixi` varchar(45) DEFAULT NULL,
-  `zaijingneichuliqingkuang` varchar(45) DEFAULT NULL,
+  `zaijingneichuliqingkuang` text,
   `canyuhuodong` varchar(45) DEFAULT NULL,
   `fumianziliao` varchar(45) DEFAULT NULL,
   `yingwenming` varchar(45) DEFAULT NULL,
   `xingming` varchar(45) DEFAULT NULL,
+  `tupiandizhi` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
